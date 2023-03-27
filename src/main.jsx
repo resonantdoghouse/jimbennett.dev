47ccf9f857deb5b64ae1faef902b02afa94e1658
Jim <jim@advancebass.com>
2023-03-27 07:30:47 -0700

    add logrocket

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import LogRocket from 'logrocket';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+
+LogRocket.init('fskgrn/personal-site');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
