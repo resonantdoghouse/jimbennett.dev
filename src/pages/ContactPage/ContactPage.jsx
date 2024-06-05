02f9cf762be3eafb12babd622df8a595d2357e28
Jim <jim@advancebass.com>
2024-06-05 08:47:58 -0700

    remove contact form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -32,41 +32,6 @@ const ContactPage = () => (
         </a>
       </li>
     </ul>
-    <h2>Contact</h2>
-    <form className="contact-form" name="contact" method="POST" netlify>
-      <label className="contact-form__label">
-        Your Name:{' '}
-        <input
-          className="contact-form__input"
-          type="text"
-          name="name"
-          placeholder="name"
-        />
-      </label>
-
-      <label className="contact-form__label">
-        Your Email:{' '}
-        <input
-          className="contact-form__input"
-          type="email"
-          name="email"
-          placeholder="email"
-        />
-      </label>
-
-      <label className="contact-form__label">
-        Message:{' '}
-        <textarea
-          className="contact-form__textarea"
-          name="message"
-          placeholder="message"
-        ></textarea>
-      </label>
-
-      <button className="contact-form__btn" type="submit">
-        Send
-      </button>
-    </form>
   </section>
 );
 
